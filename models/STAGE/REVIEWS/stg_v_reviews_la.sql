1f08c42f89d1b5e9fb0cfe3c5f2f6366170cab38
anusha s <anushaslearning4@gmail.com>
2024-10-04 20:59:41 +0000

    sdf

--- models/STAGE/REVIEWS/stg_v_reviews_la.sql
+++ models/STAGE/REVIEWS/stg_v_reviews_la.sql
@@ -1,4 +1,4 @@
-select 'LA' as Country, * from  {{ source('crd', 'RAW_REVIEW_LA') }}
+select 'LA' as Country_txt, * from  {{ source('crd', 'RAW_REVIEW_LA') }}
 where filename in 
 (
 select distinct METADATA$FILENAME from @RAW_CRD_DATABASE.RAW_CRD_SCHEMA.ext_stage/reviews/reviews_la
--- models/STAGE/REVIEWS/stg_v_reviews_na.sql
+++ models/STAGE/REVIEWS/stg_v_reviews_na.sql
@@ -1,4 +1,4 @@
-select 'NA' as Country, * from  {{ source('crd', 'RAW_REVIEW_NA') }}
+select 'NA' as Country_txt, * from  {{ source('crd', 'RAW_REVIEW_NA') }}
 where filename in 
 (
 select distinct METADATA$FILENAME from @RAW_CRD_DATABASE.RAW_CRD_SCHEMA.ext_stage/reviews/reviews_na
